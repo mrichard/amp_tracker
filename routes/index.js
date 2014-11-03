c8c32304ee3273700790e12c9b19ef90aa661f86
Michel Richard <mrichard4@LM3000026417.local>
2014-11-02 19:14:58 -0500

    root route

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 module.exports = function(app){
+	app.get( '/', function( req, res ){
+		res.status( 200 ).json('hello world');
+
+	});
 	app.use( '/heartbeat', require( './heartbeat' ) );
 	app.use( '/track', require( './amptracker') );
 	app.use( '/index', require( './main' ) );
